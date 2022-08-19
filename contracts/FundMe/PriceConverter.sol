c08807592f97e570a55e29ee45bfa9125a20c77c
Remix Workspace <>
2022-08-19 17:30:09 +0200

    add

--- contracts/FundMe/PriceConverter.sol
+++ contracts/FundMe/PriceConverter.sol
@@ -11,21 +11,29 @@ library PriceConverter {
         // Rinkeby ETH / USD Address
         // https://docs.chain.link/docs/ethereum-addresses/
         AggregatorV3Interface priceFeed = AggregatorV3Interface(
-            0x8A753747A1Fa494EC906cE90E9f37563A8AF630e
+            0xD4a33860578De61DBAbDc8BFdb98FD742fA7028e
         );
-        (, int256 answer, , , ) = priceFeed.latestRoundData();
+        (, int256 price, , , ) = priceFeed.latestRoundData();
         // ETH/USD rate in 18 digit
-        return uint256(answer * 10000000000);
+         // ETH in terms of USD
+        //returns 3000.00000000 ( 8 decimals ) we need 18 decimals
+        return uint256(price * 1e10); // 1**10
+    }
+    
+
+    function getVersion() internal view returns (uint256){
+      // get the address of the right contract here  :https://docs.chain.link/docs/ethereum-addresses/
+      AggregatorV3Interface priceFeed = AggregatorV3Interface(0xD4a33860578De61DBAbDc8BFdb98FD742fA7028e);
+      return priceFeed.version();
     }
 
-    // 1000000000
     function getConversionRate(uint256 ethAmount)
         internal
         view
         returns (uint256)
     {
         uint256 ethPrice = getPrice();
-        uint256 ethAmountInUsd = (ethPrice * ethAmount) / 1000000000000000000;
+        uint256 ethAmountInUsd = (ethPrice * ethAmount) / 1000000000000000000; // 1e18
         // the actual ETH/USD conversion rate, after adjusting the extra 0s.
         return ethAmountInUsd;
     }
